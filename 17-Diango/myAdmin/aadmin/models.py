76c26bbb0a76d2f8767214b7941c3701ee3179d5
陈孙旭 <chensunxu@163.com>
2019-01-04 16:46:35 +0800

    6-14-admin

--- 17-Diango/myAdmin/aadmin/models.py
+++ 17-Diango/myAdmin/aadmin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import time
 
 # Create your models here.
 class ClassRoom(models.Model):
@@ -14,6 +15,18 @@ class Teacher(models.Model):
     course = models.CharField(max_length=20)
 
     room = models.OneToOneField(ClassRoom)
+
+    def getRoomName(self):
+        return self.room.roomName
+
+    getRoomName.short_description = "教室"
+
+    def curTime(self):
+        return time.time()
+
+    curTime.short_description = "当前时间"
+    curTime.admin_order_field = "name"
+
     def __str__(self):
         return self.name
 
